e66e1ceb7277b492bd562a78722053a8476f16e7
huchuchu <udahyun0205@email.com>
2019-12-12 00:00:27 +0900

    delete

--- chapter3/number.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    
-    const a = parseInt("16 vote", 10) //vote는 무시된다. 10진수 16
-    const b = parseInt("3a",16) //16진수 3a를 10진수로 변환
-    const c = parseFloat("15.5 kph") //kph무시. parseFloat은 항상 기수가 10이라고 가정함
-
-});
