09ed0611f65f3e31af80fc88c08b72a5b5035399
huchuchu <udahyun0205@gmail.com>
2019-12-16 14:56:04 +0900

    chapter7

--- chapter7/IIFF.js
+++ chapter7/IIFF.js
@@ -1,18 +1,23 @@
-const message = (function(){
-    const secret = "I'm a serect!"
-    return 'The secret is ${secret.length} characters long .';
 
-})();
-console.log(message);
+$(document).ready(function(){
 
-//_2
+    const message = (function(){
 
-const f = (function(){
-    let count = 0;
-    return function(){
-        return 'I have been called ${++count} time(s).';
-    }
-})();
+        const secret = "I'm a secret!";
+        return `The secret is ${secret.length} characters long`;
+    
+    })();
+    // console.log(message);
 
-f();
-f();
+    const f = (function(){
+        let cnt = 0;
+        return function(){
+            return `I have been called ${++cnt} time(s)`;
+        }
+    })();
+    console.log(f());
+    console.log(f());
+
+
+});
+
